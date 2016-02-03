9e22b515a9f95994a19f7b3e45f537f5e74f3531
Wijnand Warren <god@ansuz.nl>
2016-02-03 15:44:32 +0100

    #8 Adding Coveralls plugin.

---
+++ sdk/src/androidTest/java/us/ledge/link/sdk/sdk/tests/robolectric/tests/models/userdata/PersonalInformationModelTest.java
@@ -0,0 +1,14 @@
+package us.ledge.link.sdk.sdk.tests.robolectric.tests.models.userdata;
+
+import android.support.test.runner.AndroidJUnit4;
+import us.ledge.link.sdk.sdk.models.userdata.PersonalInformationModel;
+
+/**
+ * Tests the {@link } class.
+ * @author Wijnand
+ */
+public class PersonalInformationModelTest {
+
+    private PersonalInformationModel mModel;
+
+}
