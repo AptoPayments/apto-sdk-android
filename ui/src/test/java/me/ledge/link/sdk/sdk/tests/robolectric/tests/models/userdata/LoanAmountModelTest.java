affc510263483bb2144756dac27ee14b9982bcd5
Wijnand Warren <god@ansuz.nl>
2016-02-23 17:42:57 +0100

    #18 Adding more tests for the 'LoanAmountModel' class.

--- ui/src/test/java/me/ledge/link/sdk/sdk/tests/robolectric/tests/models/userdata/LoanAmountModelTest.java
+++ ui/src/test/java/me/ledge/link/sdk/sdk/tests/robolectric/tests/models/userdata/LoanAmountModelTest.java
@@ -1,10 +1,13 @@
 package me.ledge.link.sdk.sdk.tests.robolectric.tests.models.userdata;
 
-import org.hamcrest.core.IsEqual;
+import me.ledge.link.sdk.ui.R;
+import me.ledge.link.sdk.ui.vos.UserDataVo;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import me.ledge.link.sdk.ui.models.userdata.LoanAmountModel;
+
+import static org.hamcrest.core.IsEqual.equalTo;
 
 /**
  * Tests the {@link LoanAmountModel} class.
@@ -33,13 +36,76 @@ public class LoanAmountModelTest {
 
     /**
      * Given an empty Model.<br />
+     * When setting min AND max values.<br />
+     * Then min and max should be properly stored.
+     */
+    @Test
+    public void minMaxAreSet() {
+        Assert.assertThat("Incorrect minimum amount.", mModel.getMinAmount(), equalTo(MIN_AMOUNT));
+        Assert.assertThat("Incorrect maximum amount.", mModel.getMaxAmount(), equalTo(MAX_AMOUNT));
+    }
+
+    /**
+     * Given an empty Model.<br />
+     * When fetching the title resource ID.<br />
+     * Then the correct ID should be returned.
+     */
+    @Test
+    public void hasCorrectTitleResource() {
+        Assert.assertThat("Incorrect resource ID.",
+                mModel.getActivityTitleResource(),
+                equalTo(R.string.loan_amount_label));
+    }
+
+    /**
+     * Given an empty Model.<br />
+     * When checking if all data has been set.<br />
+     * Then the result should be false.
+     */
+    @Test
+    public void noDataSet() {
+        Assert.assertFalse("There should be missing data.", mModel.hasAllData());
+    }
+
+    /**
+     * Given an empty Model.<br />
+     * When setting base data with a valid amount.<br />
+     * Then the amount in the Model should be the same as in the base data.
+     */
+    @Test
+    public void settingBaseDataUpdatesAmount() {
+        UserDataVo base = new UserDataVo();
+        base.loanAmount = EXPECTED_VALID_AMOUNT;
+
+        mModel.setBaseData(base);
+
+        Assert.assertTrue("All data should be present.", mModel.hasAllData());
+        Assert.assertThat("Incorrect amount.", mModel.getAmount(), equalTo(base.loanAmount));
+    }
+
+    /**
+     * Given a Model with the amount set.<br />
+     * When fetching the base data.<br />
+     * Then the base data should contain the same amount as the Model.
+     */
+    @Test
+    public void baseDataIsUpdated() {
+        mModel.setBaseData(new UserDataVo());
+
+        mModel.setAmount(EXPECTED_VALID_AMOUNT);
+
+        Assert.assertThat("Incorrect amount.", mModel.getBaseData().loanAmount, equalTo(mModel.getAmount()));
+    }
+
+    /**
+     * Given an empty Model.<br />
      * When trying to set a valid loan amount.<br />
      * Then the amount should be properly stored.
      */
     @Test
     public void validAmountIsStored() {
         mModel.setAmount(EXPECTED_VALID_AMOUNT);
-        Assert.assertThat("Amount should be stored.", mModel.getAmount(), IsEqual.equalTo(EXPECTED_VALID_AMOUNT));
+        Assert.assertThat("Amount should be stored.", mModel.getAmount(), equalTo(EXPECTED_VALID_AMOUNT));
     }
 
     /**
@@ -53,7 +119,7 @@ public class LoanAmountModelTest {
         mModel.setAmount(TOO_SMALL_AMOUNT);
 
         Assert.assertThat("Too small amount should be ignored.",
-                mModel.getAmount(), IsEqual.equalTo(EXPECTED_VALID_AMOUNT));
+                mModel.getAmount(), equalTo(EXPECTED_VALID_AMOUNT));
     }
 
     /**
@@ -67,7 +133,7 @@ public class LoanAmountModelTest {
         mModel.setAmount(TOO_LARGE_AMOUNT);
 
         Assert.assertThat("Too large amount should be ignored.",
-                mModel.getAmount(), IsEqual.equalTo(EXPECTED_VALID_AMOUNT));
+                mModel.getAmount(), equalTo(EXPECTED_VALID_AMOUNT));
     }
 
 }
