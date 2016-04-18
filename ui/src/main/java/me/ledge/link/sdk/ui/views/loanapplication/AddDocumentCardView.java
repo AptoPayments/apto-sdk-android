799cf29e1a5f80fb38f42737117a2ce622438144
Wijnand Warren <god@ansuz.nl>
2016-04-18 18:02:34 +0200

    #39 First start of the Views for the 'Add docs' screen.

---
+++ ui/src/main/java/me/ledge/link/sdk/ui/views/loanapplication/AddDocumentCardView.java
@@ -0,0 +1,52 @@
+package me.ledge.link.sdk.ui.views.loanapplication;
+
+import android.content.Context;
+import android.support.v7.widget.CardView;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+import android.widget.TextView;
+import me.ledge.link.sdk.ui.R;
+
+/**
+ * Displays a single request to add a document for a loan application.
+ * @author Wijnand
+ */
+public class AddDocumentCardView extends CardView {
+
+    private ImageView mIconView;
+    private TextView mTitleField;
+    private TextView mDescriptionField;
+
+    /**
+     * @see CardView#CardView
+     * @param context See {@link CardView#CardView}.
+     */
+    public AddDocumentCardView(Context context) {
+        super(context);
+    }
+
+    /**
+     * @see CardView#CardView
+     * @param context See {@link CardView#CardView}.
+     * @param attrs See {@link CardView#CardView}.
+     */
+    public AddDocumentCardView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    /**
+     * Finds all references to child Views.
+     */
+    private void findAllViews() {
+        mIconView = (ImageView) findViewById(R.id.iv_icon);
+        mTitleField = (TextView) findViewById(R.id.tv_title);
+        mDescriptionField = (TextView) findViewById(R.id.tv_description);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        findAllViews();
+    }
+}
