59f021be64f9683eb7f1c6a9853ece94447f9d17
Wijnand Warren <god@ansuz.nl>
2016-05-24 12:38:53 +0200

    #68 Fixing tests.

--- sdk/src/test/java/me/ledge/link/sdk/sdk/mocks/sdk/tasks/handlers/MockResponseHandler.java
+++ sdk/src/test/java/me/ledge/link/sdk/sdk/mocks/sdk/tasks/handlers/MockResponseHandler.java
@@ -10,6 +10,18 @@ public class MockResponseHandler implements ApiResponseHandler {
 
     /** {@inheritDoc} */
     @Override
+    public void subscribe(Object target) {
+        // Do nothing.
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void unsubscribe(Object target) {
+        // Do nothing.
+    }
+
+    /** {@inheritDoc} */
+    @Override
     public <T> void publishResult(T result) {
         // Do nothing.
     }
