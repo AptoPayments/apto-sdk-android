093b085747fcf8d758201434fa6c2d3da57e1f3a
Wijnand Warren <god@ansuz.nl>
2016-06-30 18:04:19 +0200

    Removing empty test.

--- ui/src/test/java/me/ledge/link/sdk/ui/tests/robolectric/tests/presenters/userdata/MonthlyIncomePresenterTest.java
+++
@@ -1,9 +0,0 @@
-package me.ledge.link.sdk.ui.tests.robolectric.tests.presenters.userdata;
-
-/**
- * TODO: Class documentation.
- *
- * @author Wijnand
- */
-public class MonthlyIncomePresenterTest {
-}
