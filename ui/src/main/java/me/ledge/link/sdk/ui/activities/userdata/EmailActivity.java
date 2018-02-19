a248ef2542ef23bf5eec884b595e759be571e302
Pau Teruel <93.pau.teruel@gmail.com>
2018-02-19 17:24:57 +0100

    Creationg of Email: - Activity - Model - Delegate - Presenter - View

---
+++ ui/src/main/java/me/ledge/link/sdk/ui/activities/userdata/EmailActivity.java
@@ -0,0 +1,8 @@
+package me.ledge.link.sdk.ui.activities.userdata;
+
+/**
+ * Created by pauteruel on 19/02/2018.
+ */
+
+public class EmailActivity {
+}
---
+++ ui/src/main/java/me/ledge/link/sdk/ui/presenters/userdata/EmailDelegate.java
@@ -0,0 +1,12 @@
+package me.ledge.link.sdk.ui.presenters.userdata;
+
+/**
+ * Created by pauteruel on 19/02/2018.
+ */
+
+public interface EmailDelegate {
+
+    void emailStored();
+    void emailLoginOnBackPressed();
+
+}
