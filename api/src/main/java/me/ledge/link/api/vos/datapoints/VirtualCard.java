8c1e68f4594f48dc5400de7d3124ae9379679bf7
Adrian Tung <adrian@ledge.me>
2018-03-09 11:56:06 +0100

    Change VitualCard's FinancialAccountType to "Card"

--- api/src/main/java/me/ledge/link/api/vos/datapoints/VirtualCard.java
+++ api/src/main/java/me/ledge/link/api/vos/datapoints/VirtualCard.java
@@ -8,6 +8,6 @@ public class VirtualCard extends Card {
     public VirtualCard() {}
 
     public VirtualCard(String accountId, String lastFourDigits, CardNetwork type, String CardBrand, String CardIssuer, String expirationDate, String PANToken, String CVVToken, FinancialAccountState state, String balance, boolean verified) {
-        super(FinancialAccountType.VirtualCard, accountId, lastFourDigits, type, CardBrand, CardIssuer, expirationDate, PANToken, CVVToken, state, balance, verified);
+        super(FinancialAccountType.Card, accountId, lastFourDigits, type, CardBrand, CardIssuer, expirationDate, PANToken, CVVToken, state, balance, verified);
     }
 }
